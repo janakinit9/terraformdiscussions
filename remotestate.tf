416dc1e4a2daa267c206cd833766e66d80baa07c
janakinit9 <61528271+janakinit9@users.noreply.github.com>
2020-04-11 20:23:21 -0500

    Create remotestate.tf

---
+++ remotestate.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "terraform-s3" {
+
+bucket = "terraform-s3-testing"
+
+dynamodb_table = "tflocktable"        
+
+versioning {
+
+enabled = true
+
+}
+
+lifecycle {
+
+prevent_destroy = true
+
+}
+
+tags {
+
+Name = "S3 Remote store"
+
+}
+
+}
